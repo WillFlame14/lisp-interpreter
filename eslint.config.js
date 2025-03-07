53b9a2dd8e7dc9fb018ee9289a8e73e5580433a7
WillFlame <flameyanai@gmail.com>
2025-03-07 18:45:39 -0500

    Change line endings to LF

---
+++ eslint.config.js
@@ -0,0 +1,29 @@
+import globals from 'globals';
+import eslint from '@eslint/js';
+import tseslint from 'typescript-eslint';
+import parser from '@typescript-eslint/parser';
+
+export default tseslint.config(
+	eslint.configs.recommended,
+	tseslint.configs.strictTypeChecked,
+	{
+		languageOptions: {
+			globals: globals.node,
+			ecmaVersion: 'latest',
+			parser,
+			parserOptions: {
+				projectService: true,
+				tsconfigRootDir: import.meta.dirname,
+			},
+			sourceType: 'module'
+		},
+		rules: {
+			'indent': ['error', 'tab', { SwitchCase: 1, ignoredNodes: ['ConditionalExpression'] }],
+			'no-unused-vars': ['error', { argsIgnorePattern: '^_' }],
+			'semi': 'error',
+			'eol-last': 'warn',
+			'no-trailing-spaces': ['warn', { ignoreComments: true }],
+			'prefer-const': 'warn'
+		}
+	}
+);
